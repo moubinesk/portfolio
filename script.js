6e1494cc9f97286b1d30f70f82ee5eaa75a28017
moubinesk <mails.moubinesk@gmail.com>
2026-07-11 17:20:20 +0200

    maj site

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ document.addEventListener("click", (e) => {
 });
 
 // ---------- Smooth scroll for hash links ----------
-// document.addEventListener("click", (e) => {
-//   const a = e.target.closest && e.target.closest('a[href^="#"]');
-//   if (!a) return;
-//   const id = a.getAttribute("href");
-//   if (id.length <= 1) return;
-//   const el = document.querySelector(id);
-//   if (el) { e.preventDefault(); el.scrollIntoView({ behavior: "smooth" }); }
-// });
+  document.addEventListener("click", (e) => {
+   const a = e.target.closest && e.target.closest('a[href^="#"]');
+   if (!a) return;
+   const id = a.getAttribute("href");
+   if (id.length <= 1) return;
+   const el = document.querySelector(id);
+   if (el) { e.preventDefault(); el.scrollIntoView({ behavior: "smooth" }); }
+ });
 
 // ---------- Scroll reveal ----------
 const io = new IntersectionObserver((entries) => {
